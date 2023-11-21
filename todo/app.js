41f31a9dd77b538bd0f3c51b2c8d2a7a7c3e0ea9
Octavio Lara <octavioasapchi@gmail.com>
2023-11-20 22:54:21 -0400

    REFACTOR render function. ADD edit button

--- todo/app.js
+++ todo/app.js
@@ -1,37 +1,50 @@
-// form#todo-form
-// input#todo
-// span#todo-length
-// ul#todo-list
 let todoList = ["Tarea de ejemplo", "Agrega mas :)"];
 
-function renderTodoList(todos){
+function createDeleteButton(todos, index) {
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-danger";
+    deleteButton.innerText = "Eliminar";
+    deleteButton.addEventListener("click", () => {
+        const newTodos = todos.filter((element, elementIndex) => index !== elementIndex);
+        todoList = newTodos;
+        renderTodoList(todoList);
+    });
+    return deleteButton;
+}
+
+function createEditButton(index){
+    const editButton = document.createElement("button");
+    editButton.className = "btn btn-warning";
+    editButton.innerText = "Editar";
+    editButton.addEventListener("click", () => {
+        // todoList[indice] = nuevo valor
+        todoList[index] = newTodo;
+        renderTodoList(todoList);
+    })
+    return editButton;
+}
+
+function renderTodoList(todos) {
     todoUl.innerHTML = "";
     todos.forEach((todo, index) => {
         const newLi = document.createElement("li");
         newLi.innerText = todo;
         newLi.className = "list-group-item d-flex justify-content-between align-items-center";
 
-        const deleteButton = document.createElement("button");
-        deleteButton.className = "btn btn-danger";
-        deleteButton.innerText = "Eliminar";
-        deleteButton.addEventListener("click", () => {
-            // seleccionar todos menos el actual
-            const newTodos = todos.filter((element, elementIndex) => index !== elementIndex);
-            todoList = newTodos;
-            renderTodoList(todoList) // recursiva
-        })
-        
-        
-        newLi.appendChild(deleteButton);
+        const delButton = createDeleteButton(todos, index);
+        const editButton = createEditButton(index);
+
+        const buttonContainer = document.createElement("div");
+        buttonContainer.className = "d-flex gap-2";
+        buttonContainer.appendChild(delButton);
+        buttonContainer.appendChild(editButton);
+
+
+        newLi.appendChild(buttonContainer);
         todoUl.appendChild(newLi);
-    })
+    });
     todoSpan.innerText = todos.length;
 }
-
-
-
-
-
 
 let newTodo = "";
 const todoInput = document.getElementById("todo");
@@ -41,16 +54,16 @@ todoInput.addEventListener("change", (evento) => {
 
 const todoForm = document.getElementById("todo-form");
 todoForm.addEventListener("submit", (event) => {
-    event.preventDefault()
+    event.preventDefault();
     todoList.push(newTodo);
     renderTodoList(todoList);
     todoInput.value = "";
-})
+});
 
 const todoSpan = document.getElementById("todo-length");
 
 const todoUl = document.getElementById("todo-list");
 
-window.onload = function(){
+window.onload = function () {
     renderTodoList(todoList);
 }
